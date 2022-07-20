e17d47a0f6b7b2be4012ee7c0d6adfeaa10e51c5
Sulthan Asyegaf Baihaqi <sultanasyegaf8893@gmail.com>
2022-07-20 18:04:51 +0700

    menyelesaikan fungsi operator, equals, dan negative

---
+++ assets/kalkulator.js
@@ -0,0 +1,99 @@
+const calculator = {
+  displayNumber: "0",
+  operator: null,
+  firstNumber: null,
+  isWaitForSecondNumber: false,
+};
+
+function updateDisplay() {
+  document.querySelector("#displayNumber").innerText = calculator.displayNumber;
+}
+
+function clearCalculator() {
+  calculator.displayNumber = "0";
+  calculator.operator = null;
+  calculator.firstNumber = null;
+  calculator.isWaitForSecondNumber = false;
+}
+
+// mendeklarasikan fungsi inverseNumber();
+function inverseNumber() {
+  if (calculator.displayNumber === "0") {
+    return;
+  }
+}
+calculator.displayNumber = calculator.displayNumber * -1;
+
+// Mendeklarasikan fungsi operator
+function handleOperator(operator) {
+  if (!calculator.isWaitForSecondNumber) {
+    calculator.operator = operator;
+    calculator.isWaitForSecondNumber = true;
+    calculator.firstNumber = calculator.displayNumber;
+
+    // mengatur ulang nilai displayNumber supaya tombol selanjutnya dimulai dari angka pertama lagi
+    calculator.displayNumber = "0";
+  } else {
+    alert("Operator sudah ditetapkan");
+  }
+}
+
+// Mendeklarasikan fungsi kalkulasi nilai
+function performCalculation() {
+  if (calculator.firstNumber == null || calculator.operator == null) {
+    alert("Anda belum menetapkan operator");
+    return;
+  }
+
+  let result = 0;
+  if (calculator.operator === "+") {
+    result = parseInt(calculator.firstNumber) + parseInt(calculator.displayNumber);
+  } else {
+    result = parseInt(calculator.firstNumber) - parseInt(calculator.displayNumber);
+  }
+  calculator.displayNumber = result;
+}
+
+// membuat fungsi untuk memasukkan angka ke dalam nilai displayNumber
+function inputDigit(digit) {
+  if (calculator.displayNumber === "0") {
+    calculator.displayNumber = digit;
+  } else {
+    calculator.displayNumber += digit;
+  }
+}
+
+const buttons = document.querySelectorAll(".button");
+for (const button of buttons) {
+  button.addEventListener("click", function (event) {
+    // mendapatkan object yang diklik
+    const target = event.target;
+
+    // membuat tombol clear (CE)
+    if (target.classList.contains("clear")) {
+      clearCalculator();
+      updateDisplay();
+      return;
+    }
+
+    if (target.classList.contains("negative")) {
+      inverseNumber();
+      updateDisplay();
+      return;
+    }
+
+    if (target.classList.contains("equals")) {
+      performCalculation();
+      updateDisplay();
+      return;
+    }
+
+    if (target.classList.contains("operator")) {
+      handleOperator(target.innerText);
+      return;
+    }
+
+    inputDigit(target.innerText);
+    updateDisplay;
+  });
+}
